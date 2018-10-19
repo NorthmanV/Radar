c8411d7355427c2aca25505f58b024f6994ebdc3
Ruslan Akberov <ruslanakberov@gmail.com>
2018-10-19 09:12:42 +0300

    Initial Commit

---
+++ Radar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Radar
+//
+//  Created by Руслан Акберов on 19/10/2018.
+//  Copyright © 2018 Ruslan Akberov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
